ec309a0f8c0ab4c6f3c1b3a808f9fabd9c4ef794
Spencer Goodman <spencerwgoodman@icloud.com>
2018-05-09 01:19:39 +0000

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,15 @@
+
+class Post
+  attr_accessor :title
+
+  def initialize
+    @title = title
+  end
+
+  def author(name)
+    self.author = name
+  end
+
+
+
+end
---
+++ lib/song.rb
@@ -0,0 +1,10 @@
+
+class Song
+  attr_accessor :title
+
+  def initialize
+    @title = title
+  end
+
+
+end
